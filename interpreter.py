897b7d783606960300c1b9ec97408664caea0bb6
Karalioki <mikheilkushashvili@gmail.com>
2020-06-12 00:17:00 +0400

    seperate lexer and interpreter
    just for mult and div

--- interpreter.py
+++ interpreter.py
@@ -1,95 +1,53 @@
 from type import Type
 from token import Token
+from lexer import Lexer
 
 class Interpreter():
 
-    def __init__(self, text):
-        self.text = text
-        self.pos = 0
-        self.current_token = None
-        self.current_char = self.text[self.pos]
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
 
     def error(self):
-        raise Exception('Error while parsing input')
+        raise Exception('Invalid syntax')
 
-    def advance(self):
-        self.pos += 1
-        if self.pos > len(self.text) - 1:
-            self.current_char = None
+    def eat(self, type):
+        # compare the current token type with the passed token
+        # type and if they match then "eat" the current token
+        # and assign the next token to the self.current_token,
+        # otherwise raise an exception.
+        if(self.current_token.type == type):
+            self.current_token = self.lexer.get_next_token()
         else:
-            self.current_char = self.text[self.pos]
-        
-    def skip_whitespace(self):
-        while self.current_char is not None and self.current_char.isspace():
-            self.advance()
-        
-    def integer(self):
-        result = ''
-        while self.current_char is not None and self.current_char.isdigit():
-            result += self.current_char
-            self.advance()
-        
-        return int(result)
+            self.error()
+    
+    def factor(self):
+        """Return an INTEGER token value.
 
-    def term(self):
+        factor : INTEGER
+        """
         token = self.current_token
         self.eat(Type.INTEGER)
         return token.value
     
-    def get_next_token(self):
-        
-        while self.current_char is not None:
-
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
-            
-            if self.current_char.isdigit():
-                return Token(Type.INTEGER, self.integer())
-
-            if self.current_char == '+':
-                self.advance()
-                return Token(Type.PLUS, '+')
-
-            if self.current_char == '-':
-                self.advance()
-                return Token(Type.MINUS, '-')
-            
-            if self.current_char == '*':
-                self.advance()
-                return Token(Type.MULT, '*')
-            
-            if self.current_char == '/':
-                self.advance()
-                return Token(Type.DIV, '/')
-            
-            self.error()
-        
-        return Token(Type.EOF, None)
-
-    def eat(self, type):
-        if self.current_token.type == type:
-            
-            self.current_token = self.get_next_token()
-        else:
-            self.error()
-
     def expr(self):
-        self.current_token = self.get_next_token()
+        """Arithmetic expression parser / interpreter.
 
-        result = self.term()
-        while self.current_token.type in (Type.PLUS, Type.MINUS):
+        expr   : factor ((MUL | DIV) factor)*
+        factor : INTEGER
+        """
+        result = self.factor()
+        while self.current_token.type in (Type.MULT, Type.DIV):
             token = self.current_token
-
-            if(token.type == Type.PLUS):
-                self.eat(Type.PLUS)
-                result = result + self.term()
-            elif token.type == Type.MINUS:
-                self.eat(Type.MINUS)
-                result = result - self.term()
-        
+            if token.type == Type.MULT:
+                self.eat(Type.MULT)
+                result = result * self.factor()
+            elif token.type == Type.DIV:
+                self.eat(Type.DIV)
+                result = result / self.factor()
+            
         return result
-    
+
 def main():
     while True:
         try:
@@ -97,15 +55,16 @@ def main():
             if(text == 'exit'):
                 break
         except EOFError:
-            print("Error Occured")
             break
         
         if not text:
             continue
-
-        interpreter = Interpreter(text)
+        
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
+
 
 
 if __name__ == "__main__":
---
+++ lexer.py
@@ -0,0 +1,65 @@
+from type import Type
+from token import Token
+
+class Lexer():
+
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_char = self.text[self.pos]
+
+    def error(self):
+        raise Exception('Invalid character')
+
+    def advance(self):
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+    
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+    
+    def integer(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        
+        return int(result)
+
+    def get_next_token(self):
+        
+        while self.current_char is not None:
+            
+            if self.current_char.isspace():
+                self.skip_whitespace()
+                continue
+            
+            if self.current_char.isdigit():
+                return Token(Type.INTEGER, self.integer())
+            
+            if self.current_char == "+":
+                self.advance()
+                return Token(Type.PLUS, "+")
+
+            if self.current_char == '-':
+                self.advance()
+                return Token(Type.MINUS, '-')
+            
+            if self.current_char == '*':
+                self.advance()
+                return Token(Type.MULT, '*')
+            
+            if self.current_char == '/':
+                self.advance()
+                return Token(Type.DIV, '/')
+            
+            self.error()
+        
+        return Token(Type.EOF, None)
+
+
+
